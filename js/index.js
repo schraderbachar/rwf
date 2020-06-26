63bf8bdec5daf6005e18f4ee79513293c07dfff7
JJ SchraderBachar <jj.schraderbachar@icloud.com>
2020-06-26 17:42:19 -0500

    changed background for landing page and changed carousel to cards

--- js/index.js
+++ js/index.js
@@ -2,13 +2,9 @@
   $(document).ready(function(){
     //scrollspy
     $('.scrollspy').scrollSpy();
-    //carousel
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true
-    });
     //modal
     $('.modal').modal();
   });
 
+  
 
