15a57a533ee15c1c55d050e3b2ed8987f47342ef
schraderbachar <jj.schraderbachar@icloud.com>
2021-12-31 14:15:49 -0600

    added espanol text on click

--- js/index.js
+++ js/index.js
@@ -3,5 +3,45 @@
     $('.scrollspy').scrollSpy();
   });
 
+  function convertToSpanish() {
+    var banner = document.getElementById("bannerText")
+    banner.innerHTML = "RWF Media: Te guiamos al éxito"
+
+    var tourText = document.getElementById("tour")
+    tourText.innerHTML = "Toma un tour" 
+
+    var startText = document.getElementById("start")
+    startText.innerHTML = "Empecemos" 
+
+    var qstext = document.getElementById("qs")
+    qstext.innerHTML = "Quiénes somos"
+
+    var jText = document.getElementById("JJ")
+    jText.innerHTML = "JJ es un desarrollador web y gestor de redes sociales quien ama crear sitios web y crecer tus redes sociales. Especialmente ama cuando crece tu negocio y tener el reconocimiento que merece y cumplir sus metas. También habla español y alemán"
+
+    var workText = document.getElementById("work")
+    workText.innerHTML = "Ve sus proyectos"    
+    
+    var offerText = document.getElementById("offer")
+    offerText.innerHTML = "Que ofrecemos"
+
+    var wdText = document.getElementById("wd")
+    wdText.innerHTML = "Desarollamiento Web"
+
+    var wdLText = document.getElementById("wdL")
+    wdLText.innerHTML = "¿Tienes un menú? ¿O nuevos productos que quieres que sepan tus clientes? Algunas ofertas? Podemos mostrar todo eso y más en tu sitio web"
+
+    var smmText = document.getElementById("smm")
+    smmText.innerHTML = "Marketing de Redes Sociales"
+
+    var smmLText = document.getElementById("smmL")
+    smmLText.innerHTML = "Entendemos que quieres gestionar tus redes sociales pero no hay tiempo, tienes un negocio que te ocupa. Eso es donde llegamos. Hacemos los posts, hablamos con sus clientes y seguidores, como si fueras tú mientras tú gestionas tu negocio."
+
+    var dmText = document.getElementById("dm")
+    dmText.innerHTML = "Marketing Digital"
+
+    var dmLText = document.getElementById("dmL")
+    dmLText.innerHTML = "Los advertidos online puede ser muy acuciante y que signifca todos los acrónimos? ¿SEO? ¿PPC? ¿CPC? No te preocupes, te tenemos. Con nuestras estrategias certificados por Google, tu sitio aparecerá hasta arriba de cada búsqueda."
+  }
   
 
