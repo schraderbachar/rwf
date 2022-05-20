9bc0a753ce6efd6c41c8e6eb976a967d0898412d
John SchraderBachar <johns3@iastate.edu>
2022-05-20 12:01:06 -0500

    updated some styles and made things look better

--- js/index.js
+++ js/index.js
@@ -1,94 +1,107 @@
-  $(document).ready(function(){
-    //scrollspy
-    $('.scrollspy').scrollSpy();
-  });
+$(document).ready(function () {
+  //scrollspy
+  $('.scrollspy').scrollSpy();
+});
 
-  function convertToSpanish() {
+function convertToSpanish() {
 
-    var banner = document.getElementById("bannerText")
-    banner.innerHTML = "RWF Media: Te guiamos al éxito"
+  var banner = document.getElementById("bannerText")
+  banner.innerHTML = "RWF Media: Te guiamos al éxito"
 
-    var tourText = document.getElementById("tour")
-    tourText.innerHTML = "Toma un tour" 
+  var tourText = document.getElementById("tour")
+  tourText.innerHTML = "Toma un tour"
 
-    document.getElementById("english").style.display="initial"
-    document.getElementById("span").style.display="none"
+  document.getElementById("english").style.display = "initial"
+  document.getElementById("span").style.display = "none"
 
-    var startText = document.getElementById("start")
-    startText.innerHTML = "Empecemos" 
+  var startText = document.getElementById("start")
+  startText.innerHTML = "Empecemos"
 
-    var qstext = document.getElementById("qs")
-    qstext.innerHTML = "Quiénes somos"
+  var qstext = document.getElementById("qs")
+  qstext.innerHTML = "Quiénes somos"
 
-    var jText = document.getElementById("JJ")
-    jText.innerHTML = "JJ es un desarrollador web y gestor de redes sociales quien ama crear sitios web y crecer tus redes sociales. Especialmente ama cuando crece tu negocio y tener el reconocimiento que merece y cumplir sus metas. También habla español y alemán"
+  var jText = document.getElementById("JJ")
+  jText.innerHTML = "JJ es un desarrollador web y gestor de redes sociales quien ama crear sitios web y crecer tus redes sociales. Especialmente ama cuando crece tu negocio y tener el reconocimiento que merece y cumplir sus metas. También habla español y alemán"
 
-    var workText = document.getElementById("work")
-    workText.innerHTML = "Ve sus proyectos"    
-    
-    var offerText = document.getElementById("offer")
-    offerText.innerHTML = "Que ofrecemos"
+  var tText = document.getElementById("freetime")
+  tText.innerHTML = "Para pasar el tiempo cuando no está trabajando, a JJ le encanta pasar tiempo fuera y viajar. Sus lugares favoritos son Chile y Costa Rica. La próxima gran aventura será ir a Europa en el verano de 2022"
 
-    var wdText = document.getElementById("wd")
-    wdText.innerHTML = "Desarollamiento Web"
+  var workText = document.getElementById("work")
+  workText.innerHTML = "Ve sus proyectos"
 
-    var wdLText = document.getElementById("wdL")
-    wdLText.innerHTML = "¿Tienes un menú? ¿O nuevos productos que quieres que sepan tus clientes? Algunas ofertas? Podemos mostrar todo eso y más en tu sitio web"
+  var offerText = document.getElementById("offer")
+  offerText.innerHTML = "Que ofrecemos"
 
-    var smmText = document.getElementById("smm")
-    smmText.innerHTML = "Marketing de Redes Sociales"
+  var wdText = document.getElementById("wd")
+  wdText.innerHTML = "Desarollamiento Web"
 
-    var smmLText = document.getElementById("smmL")
-    smmLText.innerHTML = "Entendemos que quieres gestionar tus redes sociales pero no hay tiempo, tienes un negocio que te ocupa. Eso es donde llegamos. Hacemos los posts, hablamos con sus clientes y seguidores, como si fueras tú mientras tú gestionas tu negocio."
+  var wdLText = document.getElementById("wdL")
+  wdLText.innerHTML = "¿Tienes un menú? ¿O nuevos productos que quieres que sepan tus clientes? Algunas ofertas? Podemos mostrar todo eso y más en tu sitio web"
 
-    var dmText = document.getElementById("dm")
-    dmText.innerHTML = "Marketing Digital"
+  var smmText = document.getElementById("smm")
+  smmText.innerHTML = "Marketing de Redes Sociales"
 
-    var dmLText = document.getElementById("dmL")
-    dmLText.innerHTML = "Los advertidos online puede ser muy acuciante y que signifca todos los acrónimos? ¿SEO? ¿PPC? ¿CPC? No te preocupes, te tenemos. Con nuestras estrategias certificados por Google, tu sitio aparecerá hasta arriba de cada búsqueda."
-  }
+  var smmLText = document.getElementById("smmL")
+  smmLText.innerHTML = "Entendemos que quieres gestionar tus redes sociales pero no hay tiempo, tienes un negocio que te ocupa. Eso es donde llegamos. Hacemos los posts, hablamos con sus clientes y seguidores, como si fueras tú mientras tú gestionas tu negocio."
 
-  function convertToEnglish() {
-    var banner = document.getElementById("bannerText")
-    banner.innerHTML = "RWF Media: Guiding you to Digital Success"
+  var dmText = document.getElementById("dm")
+  dmText.innerHTML = "Marketing Digital"
 
-    var tourText = document.getElementById("tour")
-    tourText.innerHTML = "Take a Tour" 
+  var dmLText = document.getElementById("dmL")
+  dmLText.innerHTML = "Los advertidos online puede ser muy acuciante y que signifca todos los acrónimos? ¿SEO? ¿PPC? ¿CPC? No te preocupes, te tenemos. Con nuestras estrategias certificados por Google, tu sitio aparecerá hasta arriba de cada búsqueda."
 
-    document.getElementById("english").style.display="none"
-    document.getElementById("span").style.display="initial"
+  var calText = document.getElementById("cal")
+  calText.innerHTML = "Programar una consulta"
+}
 
-    var startText = document.getElementById("start")
-    startText.innerHTML = "Lets Start" 
+function convertToEnglish() {
+  var banner = document.getElementById("bannerText")
+  banner.innerHTML = "RWF Media: Guiding you to Digital Success"
 
-    var qstext = document.getElementById("qs")
-    qstext.innerHTML = "Who we are"
+  var tourText = document.getElementById("tour")
+  tourText.innerHTML = "Take a Tour"
 
-    var jText = document.getElementById("JJ")
-    jText.innerHTML = "JJ is an experienced web developer and social media manager who loves to create websites and see your social media pages grow. He loves seeing small businesses getting the recognition they deserve and acheive their goals. He also speaks fluent spanish and intermediate german."
+  document.getElementById("english").style.display = "none"
+  document.getElementById("span").style.display = "initial"
 
-    var workText = document.getElementById("work")
-    workText.innerHTML = "See his work"    
-    
-    var offerText = document.getElementById("offer")
-    offerText.innerHTML = "What We Offer"
+  var startText = document.getElementById("start")
+  startText.innerHTML = "Lets Start"
 
-    var wdText = document.getElementById("wd")
-    wdText.innerHTML = "Web Development"
+  var qstext = document.getElementById("qs")
+  qstext.innerHTML = "Who we are"
 
-    var wdLText = document.getElementById("wdL")
-    wdLText.innerHTML = "Have a menu? What about new products that you want your customers to know about? Upcoming Deals? We can show all of that and more on your website."
+  var jText = document.getElementById("JJ")
+  jText.innerHTML = "JJ is an experienced web developer and social media manager who loves to create websites and see your social media pages grow. He loves seeing small businesses getting the recognition they deserve and acheive their goals. He also speaks fluent spanish and intermediate german."
 
-    var smmText = document.getElementById("smm")
-    smmText.innerHTML = "Social Media Marketing"
+  var tText = document.getElementById("freetime")
+  tText.innerHTML = "In his free time JJ, loves spending time outdoors and traveling. Some of his favorite places are Chile and Costa Rica. His next big adventure is going to Europe in summer of 2022"
 
-    var smmLText = document.getElementById("smmL")
-    smmLText.innerHTML = "We get it. You have every intention of running an amazing social medial, you just don't have the time. That's where we come in. We do the posting, engaging, and connecting like you would while you run your successful business."
 
-    var dmText = document.getElementById("dm")
-    dmText.innerHTML = "Digital Marketing"
+  var workText = document.getElementById("work")
+  workText.innerHTML = "See his work"
 
-    var dmLText = document.getElementById("dmL")
-    dmLText.innerHTML = "Online ads can be very overwhelming. SEO, PPC, CPC, what does it all mean?!?!?! Don't worry we've got you covered. With our Google Certified strategies, your website will appear at the top of every search."
-  }
+  var offerText = document.getElementById("offer")
+  offerText.innerHTML = "What We Offer"
+
+  var wdText = document.getElementById("wd")
+  wdText.innerHTML = "Web Development"
+
+  var wdLText = document.getElementById("wdL")
+  wdLText.innerHTML = "Have a menu? What about new products that you want your customers to know about? Upcoming Deals? We can show all of that and more on your website."
+
+  var smmText = document.getElementById("smm")
+  smmText.innerHTML = "Social Media Marketing"
+
+  var smmLText = document.getElementById("smmL")
+  smmLText.innerHTML = "We get it. You have every intention of running an amazing social medial, you just don't have the time. That's where we come in. We do the posting, engaging, and connecting like you would while you run your successful business."
+
+  var dmText = document.getElementById("dm")
+  dmText.innerHTML = "Digital Marketing"
+
+  var dmLText = document.getElementById("dmL")
+  dmLText.innerHTML = "Online ads can be very overwhelming. SEO, PPC, CPC, what does it all mean?!?!?! Don't worry we've got you covered. With our Google Certified strategies, your website will appear at the top of every search."
+
+  var calText = document.getElementById("cal")
+  calText.innerHTML = "Schedule a consultation"
+}
 
