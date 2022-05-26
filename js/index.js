b243b44c1ebe4fb6372db203c43779412f23ac36
John SchraderBachar <johns3@iastate.edu>
2022-05-25 21:00:50 -0500

    seo wording changes

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ function convertToSpanish() {
   qstext.innerHTML = "Quiénes somos"
 
   var jText = document.getElementById("JJ")
-  jText.innerHTML = "JJ es un desarrollador web y gestor de redes sociales quien ama crear sitios web y crecer tus redes sociales. Especialmente ama cuando crece tu negocio y tener el reconocimiento que merece y cumplir sus metas. También habla español y alemán"
+  jText.innerHTML = "A JJ le encanta reunir con dueños de empresas pequeñas para crecer su presencia online y cumplir sus metas. Como dueño de una empresa pequeña el mismo, sabe lo que es dirigir y adminsitrarlo. También le encanta usar su español para connectar mejor con la comunidad LatinX en el area Des Moines."
 
   var tText = document.getElementById("freetime")
   tText.innerHTML = "Para pasar el tiempo cuando no está trabajando, a JJ le encanta pasar tiempo fuera y viajar. Sus lugares favoritos son Chile y Costa Rica. La próxima gran aventura será ir a Europa en el verano de 2022"
@@ -33,22 +33,22 @@ function convertToSpanish() {
   offerText.innerHTML = "Que ofrecemos"
 
   var wdText = document.getElementById("wd")
-  wdText.innerHTML = "Desarollamiento Web"
+  wdText.innerHTML = "Desarollamiento y Diseño Web para empresas pequeñas"
 
   var wdLText = document.getElementById("wdL")
-  wdLText.innerHTML = "¿Tienes un menú? ¿O nuevos productos que quieres que sepan tus clientes? Algunas ofertas? Podemos mostrar todo eso y más en tu sitio web"
+  wdLText.innerHTML = "¿Quieres crear un sitio web para tu empresa? ¿Quieres establacer una tienda ecommerce? Muestra ofertas o productus nuevos a tus clientes? Sabemos los tacticos para mantaner tu sitio y atraer a nuevos clientes."
 
   var smmText = document.getElementById("smm")
-  smmText.innerHTML = "Marketing de Redes Sociales"
+  smmText.innerHTML = "Marketing de Redes Sociales para empresas pequeñas"
 
   var smmLText = document.getElementById("smmL")
-  smmLText.innerHTML = "Entendemos que quieres gestionar tus redes sociales pero no hay tiempo, tienes un negocio que te ocupa. Eso es donde llegamos. Hacemos los posts, hablamos con sus clientes y seguidores, como si fueras tú mientras tú gestionas tu negocio."
+  smmLText.innerHTML = "Entendemos que quieres gestionar tus redes sociales pero no hay tiempo, tienes un negocio que te ocupa. Para eso estamos. Creamos el contenido, hacemos los posts, hablamos con sus clientes y seguidores, como si fueramos tú mientras tú gestionas tu empresa."
 
   var dmText = document.getElementById("dm")
   dmText.innerHTML = "Marketing Digital"
 
   var dmLText = document.getElementById("dmL")
-  dmLText.innerHTML = "Los advertidos online puede ser muy acuciante y que signifca todos los acrónimos? ¿SEO? ¿PPC? ¿CPC? No te preocupes, te tenemos. Con nuestras estrategias certificados por Google, tu sitio aparecerá hasta arriba de cada búsqueda."
+  dmLText.innerHTML = "Los advertidos online puede ser muy acuciante y que signifca todos los acrónimos? ¿SEO? ¿PPC? ¿CPC? No te preocupes, te tenemos. Con nuestras estrategias certificados por Google, tu sitio aparecerá hasta arriba en cada búsqueda."
 
   var calText = document.getElementById("cal")
   calText.innerHTML = "Programar una consulta"
@@ -71,7 +71,7 @@ function convertToEnglish() {
   qstext.innerHTML = "Who we are"
 
   var jText = document.getElementById("JJ")
-  jText.innerHTML = "JJ is an experienced web developer and social media manager who loves to create websites and see your social media pages grow. He loves seeing small businesses getting the recognition they deserve and acheive their goals. He also speaks fluent spanish and intermediate german."
+  jText.innerHTML = "JJ loves meeting with small business  owners to help them grow their online presence and achieve their goals. As a small business owner himself, he knows what it takes to run a small business and keep it afloat. He loves using his fluency in spanish to connect with the LatinX community in the Des Moines Area."
 
   var tText = document.getElementById("freetime")
   tText.innerHTML = "In his free time JJ, loves spending time outdoors and traveling. Some of his favorite places are Chile and Costa Rica. His next big adventure is going to Europe in summer of 2022"
@@ -84,22 +84,22 @@ function convertToEnglish() {
   offerText.innerHTML = "What We Offer"
 
   var wdText = document.getElementById("wd")
-  wdText.innerHTML = "Web Development"
+  wdText.innerHTML = "Web Design & Development"
 
   var wdLText = document.getElementById("wdL")
-  wdLText.innerHTML = "Have a menu? What about new products that you want your customers to know about? Upcoming Deals? We can show all of that and more on your website."
+  wdLText.innerHTML = "Want to create a website for your brand or store? Want to set up an ecommerce store? Showcase upcoming deals or new products? We know the best tactics to keep your website up to date to reel clients in."
 
   var smmText = document.getElementById("smm")
-  smmText.innerHTML = "Social Media Marketing"
+  smmText.innerHTML = "Social Media Marketing for Small Business"
 
   var smmLText = document.getElementById("smmL")
-  smmLText.innerHTML = "We get it. You have every intention of running an amazing social medial, you just don't have the time. That's where we come in. We do the posting, engaging, and connecting like you would while you run your successful business."
+  smmLText.innerHTML = "We get it. You want to run an amazing social medial, you just don't have the time. That's where we come in. We do the content creating, posting, engaging, and connecting like you would while you run your successful business."
 
   var dmText = document.getElementById("dm")
-  dmText.innerHTML = "Digital Marketing"
+  dmText.innerHTML = "Digital Marketing for Small Business"
 
   var dmLText = document.getElementById("dmL")
-  dmLText.innerHTML = "Online ads can be very overwhelming. SEO, PPC, CPC, what does it all mean?!?!?! Don't worry we've got you covered. With our Google Certified strategies, your website will appear at the top of every search."
+  dmLText.innerHTML = "Online ads can be very overwhelming especially when navigating it by yourself. SEO, PPC, CPC, what does it all mean?!?!?! Don't worry we've got you covered. With our Google Certified strategies, your website will appear at the top of every search."
 
   var calText = document.getElementById("cal")
   calText.innerHTML = "Schedule a consultation"
