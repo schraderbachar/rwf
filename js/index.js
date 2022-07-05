4a5789c79f62ce38c9a6a9b411f33493568eda28
schraderbachar <jj.schraderbachar@icloud.com>
2022-07-05 11:44:17 +0200

    updated navbar- wrong part is bolded on start still

--- js/index.js
+++ js/index.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   //scrollspy
   $('.scrollspy').scrollSpy();
 });
-
+//nav bar transparent while on top of page
+var navcol = $(".navbar.navbar-dark");
+var bg = "bg";
+var bannerImageHeight = $('#top').height();
+navcol.removeClass(bg);
+//add nav when not on the top
+$(window).scroll(() => {
+    $(this).scrollTop() > (bannerImageHeight - 50) ? navcol.addClass(bg) : navcol.removeClass(bg);
+});
